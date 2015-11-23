041b8a16d576b166c41ea779109fe707bddcecb5
gvop <guusvanooijen@gmail.com>
2015-11-23 13:54:16 +0000

    console version of the web_url

--- newsroom/api/controllers/guardianController.js
+++ newsroom/api/controllers/guardianController.js
@@ -1,18 +1,19 @@
 var request = require('request');
 
 var api_key = "27p9pvgpraxcfgkexvesemun"
-var url = 'theguardian.com';
 
 // Get requiest 10 articles
-
-function getArticals(){
-  request('http://content.guardianapis.com/search?api-key=' + api_key , function (err, message, data) {
+function getArticles(){
+  request('http://content.guardianapis.com/search?api-key=' + api_key , function (err, data) {
     if(err) return console.log (err)
-    if (!err && message.statusCode == 200) {
-      var newsFeed = data.response.results 
-      console.log(newsFeed);
+    if (!err && data.statusCode == 200) {
+      var dataObject = JSON.parse(data.body)
+      var i = 0;
+      for (i;i<dataObject.response.results.length;i++){
+         console.log(dataObject.response.results[i].webUrl)
+      }
     }
   });
 }
 
-getArticals()
+getArticles()
--- newsroom/api/controllers/usersController.js
+++ newsroom/api/controllers/usersController.js
@@ -9,7 +9,9 @@ function usersIndex(req, res) {
 
 function usersShow(req, res){
   User.findById(req.params.id, function(err, user){
+
     if (err) return res.status(404).json({message: 'Something went wrong.'});
+    
     res.status(200).json({ user: user });
   });
 }
