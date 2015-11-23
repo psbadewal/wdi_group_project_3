47a89e88abab7887ce27614da6cbf95e757934f7
gvop <guusvanooijen@gmail.com>
2015-11-23 15:27:22 +0000

    getting the api for articles on the right route

--- newsroom/api/controllers/guardianController.js
+++ newsroom/api/controllers/guardianController.js
@@ -1,19 +1,25 @@
 var request = require('request');
 
-var api_key = "27p9pvgpraxcfgkexvesemun"
+var api_key = process.env.THEGUARDIAN_NEWS_API
 
 // Get requiest 10 articles
-function getArticles(){
+function getArticles(req, res){
   request('http://content.guardianapis.com/search?api-key=' + api_key , function (err, data) {
     if(err) return console.log (err)
     if (!err && data.statusCode == 200) {
       var dataObject = JSON.parse(data.body)
+      res.status(200).json({ dataObject: dataObject });
+
       var i = 0;
       for (i;i<dataObject.response.results.length;i++){
          console.log(dataObject.response.results[i].webUrl)
       }
+
     }
   });
 }
 
-getArticles()
+
+module.exports = {
+  getArticles:  getArticles
+}
