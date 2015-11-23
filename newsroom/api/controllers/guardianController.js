52132618bf3ecd51876ceb3eaff9535067ece141
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-11-23 18:51:02 +0000

    Working json with articles from alyian

--- newsroom/api/controllers/guardianController.js
+++ newsroom/api/controllers/guardianController.js
@@ -8,55 +8,51 @@ var textapi = new AYLIENTextAPI({
   application_key: process.env.AYLIEN_KEY
 });
 
-
+var articles = []
 
 // Get requiest 10 articles
 function getArticles(req, res){
-  request('http://content.guardianapis.com/search?api-key=' + guardian_api_key , function (err, data) {
+  request('http://content.guardianapis.com/search?api-key=' + guardian_api_key, function (err, data) {
     if(err) return console.log (err)
       if (!err && data.statusCode == 200) {
         var dataObject = JSON.parse(data.body)
+        var dataObjectRes = dataObject.response.results
 
-      //output to api
-      // res.status(200).json({ dataObject: dataObject });
-
-      var i = 0;
-      for (i;i<dataObject.response.results.length;i++){
-       var url = dataObject.response.results[i].webUrl
-       //Get the text and picture of the article
-       aylienAtricle(req, res, url)
-
-       //Get the hashed of the article
-       aylienHashes(req,res,url)
-       
-       //Put it all together in a json hash
+        var i = 0;
+        for (i;i<dataObject.response.results.length;i++){
+          var url = dataObject.response.results[i].webUrl;
+          aylienArticle(req, res, url)
+        }
      }
-   }
- });
+   })
+res.status(200).json({ articles: articles });
 }
 
-function aylienAtricle(req, res, url){
+
+function aylienArticle(req, res, url){
   textapi.extract({
     url: url,
     best_image: true
   }, function(error, response) {
     if (error === null) {
-      return response;
-    }
-  });
-}
-function aylienHashes(req,res,url){
+      console.log(response)
+      articles.push(response)
 
-  textapi.hashtags({
-    url: url
-  }, function(error, response) {
-    if (error === null) {
-      console.log(response.hashtags);
     }
   });
 }
 
 
+// function aylienHashes(req,res,url){
+//   textapi.hashtags({
+//     url: url
+//   }, function(error, response) {
+//     if (error === null) {
+//       // console.log(response.hashtags);
+//       // return response
+//     }
+//   });
+// }
 
 module.exports = {
   getArticles:  getArticles
