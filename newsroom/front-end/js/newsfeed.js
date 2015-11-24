e36ef2ae00a157b01f70f1a8787a14e32cad58ad
Calum-Campbell <campbell.cj@hotmail.co.uk>
2015-11-24 17:55:34 +0000

    Twitter with button working

--- newsroom/front-end/js/newsfeed.js
+++ newsroom/front-end/js/newsfeed.js
@@ -16,12 +16,28 @@ var count = 0;
 function appendArticle(data) {
   count ++;
   $('#newsfeed_ul').append('<li><div class="collapsible-header"><h4>' + data.article.title +
-    '</h4></div><div class="collapsible-body"><p>'+data.article.article+'</p><img src=' + data.article.image+'><div id="hashtags_'+count+'"><button>Start Twitter</button</div></div></li>')
+    '</h4></div><div class="collapsible-body"><p>'+data.article.article+'</p><img src=' + data.article.image+'><div id="hashtags_'+count+'"><button id="twitter-button">Start Twitter</button</div></div></li>')
+
+  $( '#twitter-button' ).on("click", twitterStreamStart)
+
   var i = 0;
   for (i; i<data.hashtags.hashtags.length; i++) {
     $('#hashtags_'+count).append("<p>" + data.hashtags.hashtags[i] + "</p>")
   }
 }
+
+
+function twitterStreamStart(){
+  var socket = io('http://localhost:3000/');
+
+  socket.on('connect', function(tweet){
+   console.log("Connected!");
+  });
+
+  socket.on('tweets', function(tweet){
+   console.log(tweet)
+  });
+};
 
 // function showHashes(data) {
 //   console.log(data)
