7fbdcd96d90728225f4865302e74d46bdde41054
gvop <guusvanooijen@gmail.com>
2015-11-24 16:59:46 +0000

    changing newfeed js

--- newsroom/front-end/js/newsfeed.js
+++ newsroom/front-end/js/newsfeed.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-//Ajax get request to localhost:3000/api/articles 
-
-//Loop trough the them and display them on the site 
-//  -Make html mockup in javascript to display articles 
-
-=======
->>>>>>> bd99f21855a51e8ecdbb8d485888cbc6b62dfb5b
 $(init)
 
 
@@ -16,18 +8,6 @@ function init() {
 function loop(data){
   var i = 0;
   for (i; i < data.length; i++) {
-<<<<<<< HEAD
-  appendArticle(data[i]); 
-  }
-}
-
-function appendArticle(data) {
-  console.log(data)
-  $('#newsfeed_ul').append('<li><div class="collapsible-header"><h4>' + data.article.title +
-    '</h4></div><div class="collapsible-body"><p>'+data.article.article+'</p><img src=' + data.article.image+'></div></li>')
-}
-
-=======
   appendArticle(data[i]);
   // showHashes(data[i]) 
   }
@@ -49,17 +29,12 @@ function appendArticle(data) {
 //   test.append(data)
 // }
 
->>>>>>> bd99f21855a51e8ecdbb8d485888cbc6b62dfb5b
 function ajaxRequest() {
   return $.ajax({
     method: "get",
     url: "http://localhost:3000/api/articles",
   }).done(function(data){
-<<<<<<< HEAD
-  
-=======
   console.log(data)
->>>>>>> bd99f21855a51e8ecdbb8d485888cbc6b62dfb5b
     return loop(data)
   }).fail(function(data) {
     displayErrors(data.responseJSON.message);
